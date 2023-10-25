cbdef54af110efd08294c1cf711fbf05af7a555e
katrin <katrin@USER2.myguest.virtualbox.org>
2023-10-25 21:23:36 +0300

    added new check_files.sh

--- bash00/check_files.sh
+++ bash00/check_files.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-[ -f "data.txt" ] || echo "data.txt is missing!"
-[ -f "data.txt" ] && cp data.txt backup.txt
-[ -f "data.txt" ] || echo "data.txt is not missing!"
+[ -f "data.txt" ] || echo "data.txt is missing!" || echo "data.txt is not missing!" && cp data.txt backup.txt
+
 
